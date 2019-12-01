186342bd1f037cb3bc3223a110b4a33d60a1148f
Alex Bates <alexb8z@lawn-143-215-125-60.lawn.gatech.edu>
2019-12-01 15:11:47 -0500

    Better arrays and title

--- js/analysis/directionIndicators.js
+++ js/analysis/directionIndicators.js
@@ -2,42 +2,41 @@
 
 const additiveColor = "#ed6f68"
 const blandAdditives = [
-    "and", "in addition", "additionally", "also", "furthermore", "as well", "on top of that", 
-    "another", "or", "nor", "along with", "similarly", "moreover"
+    ", and", "in addition", "additionally", "also", "or"
  ]
  const flavorfulAdditives= [
     "in the first place", "again",
     "as a matter of fact", "together with", "of course", "likewise", "coupled with", "comparatively",
     "correspondingly", "identically", "in the same fashion", "in the same way", "in the same vein",
-    "identically", "not to mention", "by the same token", "equally important"
+    "identically", "not to mention", "by the same token", "equally important", "furthermore", "as well", "on top of that",
+    "similarly", "moreover", "along with", "another", "nor"
 ]
 
 const sequentialColor = "#e89623"
 const blandSequential = [
-    "firstly", "lastly", "next", "then", "after", "later", "from time to time",
-    "last", "first", "at the same time", "during", "since", "as soon as",
-    "before", "as long as", "now", "finally"
+    "firstly", "lastly", "next", "then", "after", "later", "now", "finally"
 ]
 const flavorfulSequential = [
-    "henceforth", "meanwhile", "until", "till", "prior to", "whenever"
+    "henceforth", "meanwhile", "until", "till", "prior to", "whenever", "from time to time", "at the same time", "as soon as", "as long as", "first", "last", "during",
+    "before"
 ]
 
 const causalColor = "#2de07e"
 const blandCausal = [
-    "therefore", "consequently", "because", "thus", "since", "hence", "due to", "as a result",
+    "consequently", "because", "since", "due to", "as a result",
     "for this reason" 
 ]
 const flavorfulCausal = [
     "accordingly", "in effect", "provided that", "granted that", "in the event that",
-    "as long as", "so long as", "unless", "only", "even if", "so as to", "so that", "in order to"
+    "as long as", "so long as", "unless", "only", "even if", "so as to", "so that", "in order to", "thus", "hence", "therfore"
 ]
 
 const oppositionalColor = "#39a8db"
 const blandOppositional = [
-    "in contrast", "on the other hand", "but", "despite", "however", "yet", "although", "even though", "while", "conversely", "instead",
+    "in contrast", "but", "however", "yet", "although", "even though", "while",
     "whereas"
 ]
 const flavorfulOppositional = [
     "notwithstanding", "nevertheless", "nonetheless", "rather", "different from", "on the contrary", "in spite of", "even so", "regardless", "then again",
-    "albeit", "above all", "in reality"
+    "albeit", "above all", "in reality", "despite", "conversely", "instead", "on the other hand"
 ]
