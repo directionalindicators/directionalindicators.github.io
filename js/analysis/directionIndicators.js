79bc4cbbf5cffe05e3afa14e02bc97fa760ef02f
directionalindicators <58398115+directionalindicators@users.noreply.github.com>
2019-12-01 12:16:35 -0500

    Initial commit

---
+++ js/analysis/directionIndicators.js
@@ -0,0 +1,32 @@
+// https://www.smart-words.org/linking-words/transition-words.html
+
+const additiveColor = "#ed6f68"
+const additives = [
+    "and", "in addition", "additionally", "also", "furthermore", "as well", "on top of that", 
+    "another", "or", "nor", "along with", "similarly", "moreover", "in the first place", "again",
+    "as a matter of fact", "together with", "of course", "likewise", "coupled with", "comparatively",
+    "correspondingly", "identically", "in the same fashion", "in the same way", "in the same vein",
+    "identically", "not to mention", "by the same token", "equally important"
+]
+
+const sequentialColor = "#e89623"
+const sequential = [
+    "firstly", "lastly", "next", "then", "after", "henceforth", "later", "from time to time",
+    "last", "first", "at the same time", "meanwhile", "until", "till", "during", "since", "as soon as",
+    "before", "prior to", "as long as", "whenever", "now", "finally"
+]
+
+const causalColor = "#2de07e"
+const causal = [
+    "therefore", "consequently", "because", "thus", "since", "hence", "due to", "as a result",
+    "for this reason", "accordingly", "in effect", "provided that", "granted that", "in the event that",
+    "as long as", "so long as", "unless", "only", "even if", "so as to", "so that", "in order to"
+]
+
+const oppositionalColor = "#39a8db"
+const oppositional = [
+    "in contrast", "on the other hand", "but", "despite", "however", "yet", "although", "even though",
+    "notwithstanding", "nevertheless", "nonetheless", "rather", "while", "conversely", "instead",
+    "whereas", "different from", "on the contrary", "in spite of", "even so", "regardless", "then again",
+    "above all", "albeit", "in reality"
+]
---
+++ js/primary.js
@@ -0,0 +1,52 @@
+function validateText() {
+    let rawContent = document.getElementById("textContent").value
+
+    if (rawContent.length <= 100) {
+        alert("Please enter at least 100 characters!")
+    } else {
+        window.sessionStorage.setItem("rawContent", rawContent)
+
+        window.location.href = "./result.html"
+    }
+    
+    return false
+}
+
+var indicatorCounts = [0, 0, 0,0]
+
+function performAnalysis() {
+    let rawContent = window.sessionStorage.getItem("rawContent")
+
+    let updatedContent = "<p>"
+    
+    additives.forEach(function(item, index) {
+        rawContent = rawContent.replace(new RegExp("\\b(" + item + ")\\b", "gi"), highlightWord("$1", additiveColor))
+    })
+    
+    sequential.forEach(function(item, index) {
+        rawContent = rawContent.replace(new RegExp("\\b(" + item + ")\\b", "gi"), highlightWord("$1", sequentialColor))
+    })
+
+    causal.forEach(function(item, index) {
+        rawContent = rawContent.replace(new RegExp("\\b(" + item + ")\\b", "gi"), highlightWord("$1", causalColor))
+    })
+
+    oppositional.forEach(function(item, index) {
+        rawContent = rawContent.replace(new RegExp("\\b(" + item + ")\\b", "gi"), highlightWord("$1", oppositionalColor))
+    })
+
+    updatedContent += rawContent
+    
+    updatedContent += "</p>"
+
+    indicatorCounts[0] = (updatedContent.match(new RegExp(additiveColor, "g")) || []).length
+    indicatorCounts[1] = (updatedContent.match(new RegExp(sequentialColor, "g")) || []).length
+    indicatorCounts[2] = (updatedContent.match(new RegExp(causalColor, "g")) || []).length
+    indicatorCounts[3] = (updatedContent.match(new RegExp(oppositionalColor, "g")) || []).length
+
+    document.getElementById("content").innerHTML = updatedContent
+}
+
+function highlightWord(word, color) {
+    return "<span style=\"background-color: " + color + "\">" + word + "</span>";
+}
